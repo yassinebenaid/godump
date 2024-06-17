f0d0c198ea6e8a9c8ab2ef5fa09f7d4ae44a6904
yassinebenaid <yassinebenaide3@gmail.com>
2024-06-17 11:16:13 +0100

    mark useless functions as deprecated

--- godump.go
+++ godump.go
@@ -8,11 +8,15 @@ func Dump(v any) error {
 }
 
 // DumpNC is just like Dump but doesn't produce any colors , useful if you want to write to a file or stream.
+//
+// Deprecated: As of v0.8.0 this function only calls [Dumper.Println]
 func DumpNC(v any) error {
 	return (&Dumper{}).Println(v)
 }
 
 // Sdump is just like Dump but returns the result instead of printing to STDOUT
+//
+// Deprecated: As of v0.8.0 this function only calls [Dumper.Sprint]
 func Sdump(v any) string {
 	d := Dumper{}
 	d.theme = defaultTheme
@@ -20,6 +24,8 @@ func Sdump(v any) string {
 }
 
 // SdumpNC is just like DumpNC but returns the result instead of printing to STDOUT
+//
+// Deprecated: As of v0.8.0 this function only calls [Dumper.Sprint]
 func SdumpNC(v any) string {
 	return (&Dumper{}).Sprint(v)
 }
